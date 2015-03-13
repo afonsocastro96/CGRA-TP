7a90dd93c54e3fa10b2a415078f91eadb5de81a3
sceptross <afonso.castro9@gmail.com>
2015-03-13 17:32:17 +0000

    Cenas da aula

--- TP2/tp2/LightingScene.js
+++ TP2/tp2/LightingScene.js
@@ -3,7 +3,7 @@ var degToRad = Math.PI / 180.0;
 var BOARD_WIDTH = 6.0;
 var BOARD_HEIGHT = 4.0;
 
-var BOARD_A_DIVISIONS = 1;
+var BOARD_A_DIVISIONS = 30;
 var BOARD_B_DIVISIONS = 100;
 
 function LightingScene() {
@@ -40,8 +40,8 @@ LightingScene.prototype.init = function(application) {
 	this.materialA = new CGFappearance(this);
 	this.materialA.setAmbient(0.3,0.3,0.3,1);
 	this.materialA.setDiffuse(0.6,0.6,0.6,1);
-	this.materialA.setSpecular(0.2,0.2,0.2,1);
-	this.materialA.setShininess(10);
+	this.materialA.setSpecular(0.0,0.0,0.8,1);
+	this.materialA.setShininess(120);
 
 	this.materialB = new CGFappearance(this);
 	this.materialB.setAmbient(0.3,0.3,0.3,1);
@@ -56,23 +56,39 @@ LightingScene.prototype.initCameras = function() {
 };
 
 LightingScene.prototype.initLights = function() {
-	this.setGlobalAmbientLight(0.5,0.5,0.5, 1.0);
+	this.setGlobalAmbientLight(0.0,0.0,0.0, 1.0);
+	//this.setGlobalAmbientLight(0.5,0.5,0.5, 1.0);
+
 
 	this.shader.bind();
 	
 	// Positions for four lights
 	this.lights[0].setPosition(4, 6, 1, 1);
 	this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
-	//this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
-	//this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
+	this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
+	this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
 
 	this.lights[0].setAmbient(0, 0, 0, 1);
 	this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-	//this.lights[0].enable();
+	this.lights[0].setSpecular(1.0, 1.0, 0, 1.0);
+
+	this.lights[0].enable();
 
 	this.lights[1].setAmbient(0, 0, 0, 1);
 	this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
-	//this.lights[1].enable();
+	this.lights[1].enable();
+
+	this.lights[2].setSpecular(1.0,1.0,1.0,1.0);
+	this.lights[2].setConstantAttenuation(0);
+	this.lights[2].setLinearAttenuation(1);
+	this.lights[2].setQuadraticAttenuation(0);
+	this.lights[2].enable();
+
+	this.lights[3].setSpecular(1.0,1.0,1.0,1.0);
+	this.lights[3].setConstantAttenuation(0);
+	this.lights[3].setLinearAttenuation(0);
+	this.lights[3].setQuadraticAttenuation(0.2);
+	this.lights[3].enable();
 
 	this.shader.unbind();
 };
--- TP2/tp2/MyQuad.js
+++ TP2/tp2/MyQuad.js
@@ -24,7 +24,16 @@ MyQuad.prototype.initBuffers = function () {
             0, 1, 2, 
 			3, 2, 1
         ];
+    
 		
 	this.primitiveType=this.scene.gl.TRIANGLES;
+
+	this.normals = [
+			0,0,1,
+			0,0,1,
+			0,0,1,
+			0,0,1
+    ]
+
 	this.initGLBuffers();
 };
